a22bb230a5309437f3e3c70100f891380cdac892
Aviral Garg <contact@aviralgarg.com>
2020-03-31 14:29:05 -0700

    Init Commit

---
+++ src/scratchPad/SimpleLocation.java
@@ -0,0 +1,27 @@
+/**
+ * Simple Location storing Latitude and Longitude of a location
+ */
+package scratchPad;
+
+/**
+ * @author Aviral Garg
+ *
+ */
+public class SimpleLocation {
+	public double latitude;
+	public double longitude;
+	
+	public SimpleLocation(double lat, double lon) {
+		this.latitude = lat;
+		this.longitude = lon;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
