82696f66568970763ae5cf800926b01821c23b3a
Aviral Garg <contact@aviralgarg.com>
2020-04-02 16:52:33 -0700

    Week 1 - 3

---
+++ README.md
@@ -0,0 +1,18 @@
+# UCSDUnfoldingMaps
+
+## Screenshots
+
+### Real-time Earthquakes Visualization
+
+<img src="./images/real-time-earthquakes.png" width="90%">
+
+### Working with PApplet
+
+<img src="./images/assign1.png" width="40%">
+
+
+### Working with PApplet, images and shapes
+
+<img src="./images/week3_1.gif" width="40%">
+
+<img src="./images/week3_2.png" width="40%">
---
+++ src/scratchPad/week2/ArrayLocation.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package scratchPad.week2;
+
+/**
+ * @author Aviral Garg
+ *
+ */
+public class ArrayLocation {
+	private double coords[];
+	public ArrayLocation(double[] coords) {
+		this.coords = coords;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double[] coords = {5.0, 0.0};
+		ArrayLocation accra = new ArrayLocation(coords);
+		coords[0] = 32.9;
+		coords[1] = -117.2;
+		System.out.println(accra.coords[0]);
+		System.out.println(accra.coords[1]);
+
+	}
+
+}
